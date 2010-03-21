8b4abb51701dd9bb9a42430cab7a9feb66219af7
sam <sam@bigboy.(none)>
2010-03-21 11:51:54 +0100

    first commit

---
+++ htmlfilter/__init__.py
@@ -0,0 +1,115 @@
+import re
+from collections import defaultdict
+
+import rules
+
+attrs_re = re.compile(r"""\s*(\w+)\s*=\s*(["'])(.*?)(?<!\\)\2""", re.DOTALL)
+
+
+class HTMLFilter:
+    """Simple HTML white list filter.
+    
+    Usage:
+        hf = HTMLFilter()
+        filtered_html = hf.filter(html)
+    
+    The filter parses the code for < and > characters.
+    It tries to correct malformed tags and close them.
+    
+    Use it with a WYSIWYG editor on the client side
+    to convert user's < and > inputs into &lt; and &gt;
+    
+    For the tough stuff, prefer BeautifulSoup.
+    
+    """
+    
+    def __init__(self, rules=rules):
+        # default config
+        # other tags and attributes are removed
+        self.allowed = rules.TAGS
+        
+        # <tag />
+        self.non_closing = rules.NON_CLOSING
+        
+        # <blockquote><blockquote></blockquote></blockquote>
+        self.overlapping = rules.OVERLAPPING
+        
+    def filter(self, data):
+        # reset
+        filtered_data = self.filtered_data = []
+        open_tags = self.open_tags = defaultdict(int)
+        handle_data = self.handle_data
+
+        chunks = data.split('<')
+        filtered_data.append(chunks.pop(0))
+        
+        for chunk in chunks:
+            handle_data(chunk)
+            
+        # close open tags
+        for tag, times in open_tags.iteritems():
+            for i in range(times):
+                filtered_data.extend(['</', tag, '>'])
+                
+        return ''.join(self.filtered_data)
+        
+    def handle_data(self, chunk):
+        if chunk:
+            if '>' in chunk:
+                tagdata, text = chunk.split('>', 1)
+            else:
+                # the tag didn't end
+                tagdata, text = chunk, ''
+                
+            self.handle_tag(tagdata)
+            self.filtered_data.append(text)
+        
+    def handle_tag(self, tagdata):
+        attrs = tagdata.strip().split(' ', 1)
+        tag = attrs.pop(0).lower()
+        if tag:
+            if tag[0] == '/':
+                self.handle_endtag(tag[1:])
+            else:
+                if attrs:
+                    # find the attributes
+                    attrs = [(a[0], a[2]) for a in attrs_re.findall(attrs[0])]
+                self.handle_starttag(tag, attrs)
+        
+    def handle_starttag(self, tag, attrs):
+        if tag in self.allowed:
+            # open tags check
+            if tag in self.non_closing:
+                tag_tail = ' /'
+            else:
+                if tag not in self.overlapping and self.open_tags[tag] > 0:
+                    self.handle_endtag(tag)
+                self.open_tags[tag] += 1
+                tag_tail = ''
+                
+            # filter attributes
+            filtered_attrs = {}
+            for attr, val in attrs:
+                if attr in self.allowed[tag]:
+                    filterfn = "%s_%s" % (tag, attr)
+                    if hasattr(self.rules, filterfn):
+                        val = getattr(self.rules, filterfn)(val)
+                    if val:
+                        filtered_attrs[attr] = val
+                    
+            self.filtered_data.extend(
+                                ['<',
+                                 tag,
+                                 filtered_attrs and ' ' or '',
+                                 ' '.join(['%s="%s"' % (k, v)
+                                           for (k, v)
+                                           in filtered_attrs.iteritems()]),
+                                 tag_tail,
+                                 '>'
+                                 ])
+    
+    def handle_endtag(self, tag):
+        if tag in self.allowed and self.open_tags[tag] > 0 and tag not in\
+        self.non_closing:
+            self.filtered_data.extend(['</', tag, '>'])
+            self.open_tags[tag] -= 1
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(name='htmlfilter',
+      version='0.1',
+      description="White list HTML Filter",
+      long_description="",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Samuel Adam',
+      author_email='',
+      url='',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=True,
+      )
